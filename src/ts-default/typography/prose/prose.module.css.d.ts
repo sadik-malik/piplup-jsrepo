4878f408a2224d00c2e3ffa07e0be4fae2e8648b
sadik-malik <malik0786sam@gmail.com>
2025-08-11 20:49:25 +0530

    feat: added typed-css-modules support and updated prose to use it

---
+++ src/ts-default/typography/prose/prose.module.css.d.ts
@@ -0,0 +1,7 @@
+declare const styles: {
+  readonly "root": string;
+  readonly "root--container": string;
+  readonly "root--noMaxWidth": string;
+};
+export = styles;
+
--- src/ts-default/typography/prose/prose.tsx
+++ src/ts-default/typography/prose/prose.tsx
@@ -1,15 +1,26 @@
-
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
 import clsx from 'clsx';
 import classes from './prose.module.css';
 
-export interface ProseProps extends React.HTMLAttributes<HTMLDivElement>{
+export interface ProseProps extends React.HTMLAttributes<HTMLDivElement> {
   asChild?: boolean;
-};
+  enableGutter?: boolean;
+  enableProse?: boolean;
+}
 
+export default function Prose(props: ProseProps): React.JSX.Element {
+  const { asChild, enableGutter = true, enableProse = true, ...rest } = props;
 
-export default function Prose({ asChild, ...props }: ProseProps): React.JSX.Element {
-  const Comp = asChild ? Slot : "div";
-  return <Comp className={clsx(classes.prose)} {...props} />;
+  const Comp = asChild ? Slot : 'div';
+  return (
+    <Comp
+      className={clsx({
+        [classes.root]: enableProse,
+        [classes['root--container']]: enableGutter,
+        [classes['root--noMaxWidth']]: !enableGutter,
+      })}
+      {...rest}
+    />
+  );
 }
