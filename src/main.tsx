24daad4f76d5b5b84bc6c28f8da5af424aff161b
sadik-malik <malik0786sam@gmail.com>
2025-08-12 10:31:18 +0530

    docs: fix case sensitive typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './docs/App';
+import App from './docs/app';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(<App />);
